8fedda5d1025cb58c7e2dad9932bcd6e99a27473
tomon9086 <tomo.mouse.9086@gmail.com>
2018-03-13 20:37:55 +0900

    :sparkles: 動画対応したと思う

---
+++ chunked-media-upload_backup.js
@@ -0,0 +1,74 @@
+// Example from https://github.com/desmondmorris/node-twitter/tree/master/examples#chunked-media
+
+const Twitter     = require('twitter');
+const client      = new Twitter({ /** ... **/ });
+
+const pathToMovie = '/path/to/your/video/animated-gif.gif';
+const mediaType   = 'image/gif'; // `'video/mp4'` is also supported
+const mediaData   = require('fs').readFileSync(pathToMovie);
+const mediaSize    = require('fs').statSync(pathToMovie).size;
+
+initUpload() // Declare that you wish to upload some media
+  .then(appendUpload) // Send the data for the media
+  .then(finalizeUpload) // Declare that you are done uploading chunks
+  .then(mediaId => {
+    // You now have an uploaded movie/animated gif
+    // that you can reference in Tweets, e.g. `update/statuses`
+    // will take a `mediaIds` param.
+  });
+
+  /**
+   * Step 1 of 3: Initialize a media upload
+   * @return Promise resolving to String mediaId
+   */
+  function initUpload () {
+    return makePost('media/upload', {
+      command    : 'INIT',
+      total_bytes: mediaSize,
+      media_type : mediaType,
+    }).then(data => data.media_id_string);
+  }
+
+  /**
+   * Step 2 of 3: Append file chunk
+   * @param String mediaId    Reference to media object being uploaded
+   * @return Promise resolving to String mediaId (for chaining)
+   */
+  function appendUpload (mediaId) {
+    return makePost('media/upload', {
+      command      : 'APPEND',
+      media_id     : mediaId,
+      media        : mediaData,
+      segment_index: 0
+    }).then(data => mediaId);
+  }
+
+  /**
+   * Step 3 of 3: Finalize upload
+   * @param String mediaId   Reference to media
+   * @return Promise resolving to mediaId (for chaining)
+   */
+  function finalizeUpload (mediaId) {
+    return makePost('media/upload', {
+      command : 'FINALIZE',
+      media_id: mediaId
+    }).then(data => mediaId);
+  }
+
+  /**
+   * (Utility function) Send a POST request to the Twitter API
+   * @param String endpoint  e.g. 'statuses/upload'
+   * @param Object params    Params object to send
+   * @return Promise         Rejects if response is error
+   */
+  function makePost (endpoint, params) {
+    return new Promise((resolve, reject) => {
+      client.post(endpoint, params, (error, data, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
--- index.js
+++ index.js
@@ -15,19 +15,56 @@ module.exports = {
 			exclude_replies
 		})
 	},
-	postStatus: async (status, medias) => {
-		let media_ids = undefined
-		if(medias instanceof Array) media_ids = medias.join(",")
-		if(typeof medias === "string") media_ids = medias
-		if(!media_ids) media_ids = undefined
+	/**
+	 *	postStatus()
+	 *	
+	 *	@param String status <required>
+	 *	@param Array medias <optional>
+	 *	@param String in_reply_to_status_id <optional>
+	 *	
+	 *	@return Object tweet?
+	 */
+	postStatus: async (status, medias, in_reply_to_status_id) => {
+		if(typeof medias === "string") in_reply_to_status_id = medias
+		const media_ids = medias instanceof Array && !medias.join(",") ? medias.join(",") : undefined
+		// if(typeof medias === "string") media_ids = medias
+		if(isNaN(Number(in_reply_to_status_id))) return
 		return await client.post("statuses/update", {
 			status,
-			media_ids
+			media_ids,
+			in_reply_to_status_id
 		})
 	},
-	postMedia: async (buffer) => {
+	postImage: async (buffer) => {
 		return await client.post("media/upload", {
 			media: buffer
 		})
+	},
+	postMP4: async function(buffer) => {
+		return await this._postMedia(buffer, "video/mp4")
+	},
+	postGif: async function(buffer) => {
+		return await this._postMedia(buffer, "image/gif")
+	},
+	_postMedia: async (buffer, media_type) => {
+		const makePost = async (opt) => {
+			return await client.post("media/upload", opt)
+		}
+		const media_id = await makePost({
+			command: "INIT",
+			total_bytes: buffer.length,
+			media_type,
+		})
+		await makePost({
+			command: "APPEND",
+			media_id
+			media: buffer,
+			segment_index: 0
+		})
+		await makePost({
+			command: "FINALIZE",
+			media_id
+		})
+		return media_id
 	}
 }
